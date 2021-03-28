fc4e5b6bfbd5b553526e63b262b520f37bde8110
samhowes <sam@samhowes.com>
2021-03-28 10:25:54 -0400

    oops

--- .buildbuddy/make_rc.sh
+++ .buildbuddy/make_rc.sh
@@ -6,7 +6,7 @@ build --bes_backend=grpcs://cloud.buildbuddy.io
 build --remote_cache=grpcs://cloud.buildbuddy.io
 build --noremote_upload_local_results # Uploads logs & artifacts without writing to cache
 build --remote_timeout=3600
-build --remote_header=x-buildbuddy-api-key=$(BUILDBUDDY_API_KEY)
+build --remote_header=x-buildbuddy-api-key=$BUILDBUDDY_API_KEY
 build --build_metadata=ROLE=CI
 build --workspace_status_command=$(pwd)/.buildbuddy/workspace_status.sh
 EOF
