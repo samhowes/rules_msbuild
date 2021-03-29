920e022badc3a2c4472d8595487604092076e442
samhowes <sam@samhowes.com>
2021-03-29 18:25:02 -0400

    Better reporting in ci script

--- tests/runall.sh
+++ tests/runall.sh
@@ -2,10 +2,29 @@
 
 set +e
 
-# if these don't build, nothing will
-bazel build //tests/sanity //tests/HelloBazel
+echo "$TMPDIR"
 
-bazel test //tests/HelloBazel:all
+report=""
+exit_status=0
+function run() {
+  "$@"
+  this_exit=$?
+  this_str=$*
+  printf -v report "%s%s: %s\n" "$report" "$this_str" "$this_exit"
+  (( exit_status = exit_status || this_exit))
+}
+
+function fail() {
+  return $1
+}
+
+# if these don't build, nothing will
+run bazel build //tests/sanity //tests/HelloBazel
+
+run bazel test //tests/HelloBazel:all
 
 # targets that __must__ be run by itself
-bazel build //tests/sandboxing/parallel
+run bazel build //tests/sandboxing/parallel
+
+echo -n "$report"
+exit $exit_status
