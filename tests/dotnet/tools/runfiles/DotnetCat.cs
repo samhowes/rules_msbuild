e81a2a8ef9d09af0eb56261164e02f57b04a5946
samhowes <sam@samhowes.com>
2021-03-31 23:09:24 -0400

    Add a runfiles library closes #37

--- tests/dotnet/tools/runfiles/DotnetCat.cs
+++ tests/dotnet/tools/runfiles/DotnetCat.cs
@@ -8,7 +8,7 @@ namespace MyRulesDotnet.Tools.RunfilesTests
         static void Main(string[] args)
         {
             var r = Runfiles.Create();
-            var contents = File.ReadAllText(r.Rlocation("my_rules_dotnet/tests/dotnet/tools/runfiles/foo.txt"));
+            var contents = File.ReadAllText(r.Rlocation("my_rules_dotnet/tests/dotnet/tools/runfiles/netcoreapp3.1/foo.txt"));
 
             Console.WriteLine(contents);
         }
