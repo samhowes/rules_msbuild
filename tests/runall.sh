00544ccc1ac843eaef9ff61eedd4835e80355d81
samhowes <sam@samhowes.com>
2021-04-06 10:11:22 -0400

    Standard shell script

--- tests/runall.sh
+++ tests/runall.sh
@@ -15,21 +15,19 @@ function run() {
 }
 
 # if these don't build, nothing will
-run bazel build //tests/sanity //tests/examples/HelloBazel
+bazel build //tests/sanity //tests/examples/HelloBazel
 
-run bazel test //tests/examples/... \
+bazel test //tests/examples/... \
   //tests/dotnet/...
 
 # targets that __must__ be run by itself
-run bazel build //tests/sandboxing/parallel
+bazel build //tests/sandboxing/parallel
 
 # todo(#12) remove this call
-run dotnet test tests/dotnet/tools/builder
-run dotnet test tests/dotnet/tools/runfiles
+dotnet test tests/dotnet/tools/builder
+dotnet test tests/dotnet/tools/runfiles
 
-tree -l "bazel-$(basename "$(pwd)")/external" | tee bazel-out/nuget.txt
-
-echo -e "\n\n============================================ TEST REPORT ================================================="
-echo -n "$report"
-echo -e "Exiting with status: $exit_status"
+#echo -e "\n\n============================================ TEST REPORT ================================================="
+#echo -n "$report"
+#echo -e "Exiting with status: $exit_status"
 exit $exit_status
