b78ec1f343bd86e7cafa429721634d42e404c53e
samhowes <sam@samhowes.com>
2021-04-23 22:25:18 -0400

    fix windows

--- tests/examples/HelloWeb/run_publish_test.go
+++ tests/examples/HelloWeb/run_publish_test.go
@@ -3,15 +3,19 @@ package main
 import (
 	"github.com/samhowes/my_rules_dotnet/tests/tools/executable"
 	"github.com/samhowes/my_rules_dotnet/tests/tools/files"
+	"os"
+	"path"
 	"testing"
 )
 
 func TestRunPublishOutput(t *testing.T) {
+	dotnetPath := files.BinPath("@dotnet")
 	config := lib.TestConfig{
 		Args:           []string{files.Path("publish/netcoreapp3.1/HelloWeb.dll")},
 		ExpectedOutput: "The special value is: 42!\n",
-		Target:         files.BinPath("@dotnet"),
+		Target:         dotnetPath,
 	}
+	os.Setenv("DOTNET_CLI_HOME", path.Dir(dotnetPath))
 	t.Logf("dotnet path: %s\n", config.Target)
 	lib.CheckExecutableOutput(t, &config)
 }
