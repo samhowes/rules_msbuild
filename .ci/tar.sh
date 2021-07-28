2dcb470ee2fd838f84aa201c93b132160ff60962
Sam Howes <sam@samhowes.com>
2021-07-28 12:33:40 +0000

    sigh

--- .ci/tar.sh
+++ .ci/tar.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 tar -cvzhf bazel-out.tar.gz --exclude=*.runfiles bazel-out
-tar -czf sandbox.tar.gz --exclude=external "$(bazel info output_base)/sandbox/linux-sandbox"
+tar -czf sandbox.tar.gz "$(bazel info output_base)/sandbox/linux-sandbox"
