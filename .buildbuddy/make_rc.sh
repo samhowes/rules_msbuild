9dc196548a51d48c89fe9e20905f941ed54ee98c
samhowes <sam@samhowes.com>
2021-10-13 21:06:29 -0400

    restore buildbuddy

--- .buildbuddy/make_rc.sh
+++ .buildbuddy/make_rc.sh
@@ -13,7 +13,7 @@ fi
 cat >buildbuddy.bazelrc <<EOF
 build --bes_results_url=https://app.buildbuddy.io/invocation/
 build --bes_backend=grpcs://cloud.buildbuddy.io
-# build --remote_cache=grpcs://cloud.buildbuddy.io
+build --remote_cache=grpcs://cloud.buildbuddy.io
 build --noremote_upload_local_results # Uploads logs & artifacts without writing to cache
 build --remote_timeout=3600
 build --remote_header=x-buildbuddy-api-key=$BUILDBUDDY_API_KEY
